10e03596edf14db8958effec0b8f1678dc7a131e
Sunidhi Chauhan <chauhantaniya75@gmail.com>
2026-07-05 17:21:11 +0530

    my second commit

---
+++ 03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = "sunidhi";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33ab" => NaN
+//true => 1; 
+//false => 0;
+
+let isLoggedIn = "Sunidhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console. log(typeof booleanIsLoggedIn);
+//onsole.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false;
+// "Sunidhi" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
