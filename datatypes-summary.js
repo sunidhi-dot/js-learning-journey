76fb0061805e0b5590c310e4d0fedf0ce7da681e
Sunidhi Chauhan <chauhantaniya75@gmail.com>
2026-07-16 11:18:40 +0530

    my third commit

---
+++ datatypes-summary.js
@@ -0,0 +1,50 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non-Primitive)
+
+// Array, Object, Function
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "sunidhi",
+    age: 20,
+}
+
+const myFunction =function (){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
+c
+// https://262.ecma-international.org/5.1/#se-11.4.3
+
+Number => number
+String => string
+Boolean => boolean
+Null => object
+Undefined => undefined
+Symbol => symbol
+BigInt => bigint
+
+
+Array => Object
+Object => Object
+Function => Function
